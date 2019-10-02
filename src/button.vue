5db855844798dccdc4013a033820d7af70ea7cbc
wyx8267 <369264448@qq.com>
2019-10-02 22:06:48 +0800

    Button组件添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,68 @@
 <template>
-    <button class="g-button">hi</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg class="icon" v-if="icon" aria-hidden="true">
+            <use :xlink:href="`#icon-${icon}`" />
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
-    export default {
+export default {
+    // props: ["icon", "iconPosition"]
+    props: {
+        icon: {},
+        iconPosition: {
+            type: String,
+            default: "left",
+            validator(value){
+                return !(value !== 'left' && value !== 'right')
+            }
+        }
     }
+};
 </script>
 
 <style scoped lang="scss">
-    .g-button{
-            font-size: var(--font-size);
-            height: var(--button-height);
-            padding: 0 1em;
-            border-radius: var(--border-radius);
-            border: 1px solid var(--border-color);
-            background: var(--button-bg);
-        
-        &:hover{
-            border-color: var(--border-color-hover);
+.g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+
+    &:hover {
+        border-color: var(--border-color-hover);
+    }
+    &:active {
+        background: var(--button-active-bg);
+    }
+    &:focus {
+        outline: none;
+    }
+    > .icon {
+        order: 1;
+        margin-right: 0.3em;
+    }
+    > .content {
+        order: 2;
+    }
+    &.icon-right {
+        > .icon {
+            order: 2;
+            margin-left: 0.3em;
+            margin-right: 0;
         }
-        &:active{
-            background: var(--button-active-bg);
-        }
-        &:focus{
-            outline: none;
+        > .content {
+            order: 1;
         }
     }
+}
 </style>
