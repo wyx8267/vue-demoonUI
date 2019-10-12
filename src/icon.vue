c1cd2dd9ca3a3cd544e4740cc53295a5b426bfde
wyx8267 <369264448@qq.com>
2019-10-12 16:15:08 +0800

    添加input组件及样式

--- src/icon.vue
+++ src/icon.vue
@@ -7,6 +7,7 @@
 <script>
     import './svg'
     export default {
+        name:'DemoonIcon',
         props: ['name']
     }
 </script>
---
+++ src/input.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="wrapper" :class="{error}">
+        <input v-bind:value="value" type="text" :disabled="disabled" :readonly="readonly" />
+        <template v-if="error">
+            <icon name="error" class="icon-error"></icon>
+            <span class="errorMessage">{{error}}</span>
+        </template>
+    </div>
+</template>
+
+<script>
+import Icon from "./icon";
+export default {
+    name: "DemoonInput",
+    components: {
+        Icon
+    },
+    props: {
+        value: {
+            type: String
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        readonly: {
+            type: Boolean,
+            default: false
+        },
+        error: {
+            type: String
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+$font-size: 12px;
+$border-radius: 4px;
+$border-color: #999;
+$border-color-hover: #666;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red: #f1453d;
+$height: 32px;
+.wrapper {
+    font-size: $font-size;
+    display: inline-flex;
+    align-items: center;
+    >:not(:last-child){
+        margin-right: 0.5em;
+    }
+    &.error {
+        > input {
+            border-color: $red;
+            &:hover {
+                border-color: $red;
+            }
+        }
+        .icon-error{
+            fill:$red;
+        }
+        .errorMessage{
+            color: $red;
+        }
+    }
+    >input {
+        height: $height;
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        padding: 0 8px;
+        &:hover {
+            border-color: $border-color-hover;
+        }
+        &:focus {
+            box-shadow: inset 0 1px 3px $box-shadow-color;
+            outline: none;
+        }
+        &[disabled],
+        &[readonly] {
+            border-color: #bbb;
+            color: #bbb;
+            cursor: not-allowed;
+        }
+    }
+}
+</style>
