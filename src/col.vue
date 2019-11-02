cea2d75e12a4b18e6a6feacdcb71747560f06149
wyx8267 <369264448@qq.com>
2019-11-02 16:46:48 +0800

    完成row&col雏形

---
+++ src/col.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DemoonCol',
+        props: {
+            span: [Number, String]
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .col{
+            height: 30px;
+            background-color: #ace;
+            border: 1px solid red;
+            width: 50%;
+            $class-prefix: col-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n / 24) * 100%;
+                }
+            }
+        }
+</style>
