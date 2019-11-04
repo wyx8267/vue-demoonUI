d696e3cfa6831155c316c520a9f20a8283dfb994
wyx8267 <369264448@qq.com>
2019-11-04 20:47:55 +0800

    row & col部分重构

--- src/col.vue
+++ src/col.vue
@@ -1,12 +1,10 @@
 <template>
     <div
         class="col"
-        :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-        :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}"
+        :class="colClass"
+        :style="colStyle"
     >
-        <div style="border: 1px solid #ace; height: 40px;">
-            <slot></slot>
-        </div>
+        <slot></slot>
     </div>
 </template>
 
@@ -23,6 +21,18 @@ export default {
         return {
             gutter: 0
         };
+    },
+    computed: {
+        colClass(){
+            let {span, offset} = this
+            return [span && `col-${span}`, offset && `offset-${offset}`]
+        },
+        colStyle() {
+            return {
+                paddingLeft: this.gutter/2+'px',
+                paddingRight: this.gutter/2+'px'
+            }
+        }
     }
 };
 </script>
