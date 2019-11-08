fd50ee712a16fe6ddc52bd326f80a13dab970011
wyx8267 <369264448@qq.com>
2019-11-08 21:11:40 +0800

    完成布局组件

---
+++ src/footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="footer">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DemoonFooter'
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="header">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DemoonHeader'
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
