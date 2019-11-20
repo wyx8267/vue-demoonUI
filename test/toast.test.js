a0b6416983bdec176a808917eec420104337d46d
wyx8267 <369264448@qq.com>
2019-11-20 22:30:31 +0800

    fixed a bug

--- test/toast.test.js
+++ test/toast.test.js
@@ -27,7 +27,7 @@ describe('Toast', () => {
                 done()
             })
         })
-        it('接受 closeButton', () => {
+        it('接受 closeButton', (done) => {
             const callback = sinon.fake()
             const Constructor = Vue.extend(Toast)
             let vm = new Constructor()
@@ -41,8 +41,12 @@ describe('Toast', () => {
             }).$mount()
             let closeButton = vm.$el.querySelector('.close')
             expect(closeButton.textContent.trim()).to.eq('关闭叭')
-            closeButton.click()
-            expect(callback).to.have.been.called
+            setTimeout(() => {
+                closeButton.click()
+                expect(callback).to.have.been.called
+                done()
+            }, 200);
+
         })
         it('接受 enableHtml', () => {
             const Constructor = Vue.extend(Toast)
