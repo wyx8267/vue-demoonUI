4f9a9f358d1cc40170cfd11c8160fd96de089ba0
wyx8267 <369264448@qq.com>
2019-11-22 19:56:13 +0800

    添加tabs基本组件及相关props

---
+++ src/tabs-pane.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="tabs">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DemoonTabsPane'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
