c9d45f65a6f8b71fa649f655b9fcd451a6f49d7a
wyx8267 <369264448@qq.com>
2019-11-24 17:26:20 +0800

    支持disabled

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,60 +1,66 @@
 <template>
-    <div class="tabs-item" @click="xxx" :class="classes">
+    <div class="tabs-item" @click="onClick" :class="classes">
         <slot></slot>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'DemoonTabsItem',
-        inject: ['eventBus'],
-        data(){
+export default {
+    name: "DemoonTabsItem",
+    inject: ["eventBus"],
+    data() {
+        return {
+            active: false
+        };
+    },
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        name: {
+            type: String | Number,
+            required: true
+        }
+    },
+    computed: {
+        classes() {
             return {
-                active: false
-            }
-        },
-        props: {
-            disabled: {
-                type: Boolean,
-                default: false
-            },
-            name: {
-                type: String | Number,
-                required: true
-            }
-        },
-        computed:{
-            classes(){
-                return {
-                    active: this.active
-                }
-            }
-        },
-        created(){
-            this.eventBus.$on('update:selected', (name)=>{
-                this.active = name === this.name
-            })
-        },
-        methods: {
-            xxx(){
-                this.eventBus.$emit('update:selected', this.name, this)
-            }
+                active: this.active,
+                disabled: this.disabled
+            };
+        }
+    },
+    created() {
+        this.eventBus.$on("update:selected", name => {
+            this.active = name === this.name;
+        });
+    },
+    methods: {
+        onClick() {
+            if(this.disabled) {return}
+            this.eventBus.$emit("update:selected", this.name, this);
         }
     }
+};
 </script>
 
 <style lang="scss" scoped>
 $blue: #ace;
-.tabs-item{
+$disabled-text-color: grey;
+.tabs-item {
     flex-shrink: 0;
     padding: 0 1em;
     cursor: pointer;
     height: 100%;
     display: flex;
     align-items: center;
-    &.active{
+    &.active {
         color: $blue;
         font-weight: bold;
+    }
+    &.disabled{
+        color: $disabled-text-color;
     }
 }
 </style>
