dee901f23eee6ec53c878eed33cf5a7059a7af7c
wyx8267 <369264448@qq.com>
2019-11-26 22:30:58 +0800

    解决有overflow hidden造成的bugg

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="popover" @click="xxx">
-        <div class="content-wrapper" v-if="visible">
+    <div class="popover" @click.stop="xxx">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
@@ -16,7 +18,19 @@ export default {
     methods: {
         xxx() {
             this.visible = !this.visible;
-            console.log(1);
+            if (this.visible === true) {
+                this.$nextTick(() => {
+                    document.body.appendChild(this.$refs.contentWrapper);
+                    let { top, left } = this.$refs.triggerWrapper.getBoundingClientRect();
+                    this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
+                    this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+                    let eventHandler = () => {
+                        this.visible = false;
+                        document.removeEventListener("click", eventHandler);
+                    };
+                    document.addEventListener("click", eventHandler);
+                });
+            }
         }
     }
 };
@@ -27,12 +41,11 @@ export default {
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-        position: absolute;
-        bottom: 100%;
-        left: 0;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0,0,0,0.5)
-    }
+}
+.content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    transform: translateY(-100%);
 }
 </style>
