2f58ed19184df88b6cb978ca1e6c9115acf19c44
wyx8267 <369264448@qq.com>
2019-11-29 18:19:37 +0800

    collapse 基本样式及功能

---
+++ src/collapse.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DemoonCollapse"
+};
+</script>
+
+<style lang="scss" scoped>
+$border-radius: 4px;
+$grey: #ddd;
+.collapse {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+}
+</style>
