40eecfe2e4018075bf6b3356f31d8d0d302df8f8
wyx8267 <369264448@qq.com>
2019-12-05 15:41:55 +0800

    layout 展示页

---
+++ docs/.vuepress/components/layout-demo-2.vue
@@ -0,0 +1,58 @@
+<template>
+    <div style="padding-top: 16px;">
+        <h2>布局2</h2>
+        <p>
+            <strong>预览</strong>
+        </p>
+        <g-layout style="color: white; margin-bottom:50px; overflow: hidden;">
+            <g-header style="height: 50px; background: lightskyblue;">header</g-header>
+            <g-layout>
+                <g-sider style="height: 100px; background: #ada; width:150px;">sider</g-sider>
+                <g-content style="height: 100px; background: deepskyblue;">content</g-content>
+            </g-layout>
+            <g-footer style="height: 50px; background: lightskyblue;">footer</g-footer>
+        </g-layout>
+        <p>
+            <strong>代码</strong>
+        </p>
+        <pre><code>{{content}}</code></pre>
+    </div>
+</template>
+
+<script>
+import Layout from "../../../src/layout";
+import Header from "../../../src/header";
+import Footer from "../../../src/footer";
+import Content from "../../../src/content";
+import Sider from "../../../src/sider";
+export default {
+    components: {
+        "g-layout": Layout,
+        "g-header": Header,
+        "g-footer": Footer,
+        "g-content": Content,
+        "g-sider": Sider
+    },
+    data() {
+        return {
+            content: `
+            <g-layout style="color: white; margin-bottom:50px; overflow: hidden;">
+            <g-header style="height: 50px; background: lightskyblue;">header</g-header>
+            <g-layout>
+                <g-sider style="height: 100px; background: #ada; width:150px;">sider</g-sider>
+                <g-content style="height: 100px; background: deepskyblue;">content</g-content>
+            </g-layout>
+            <g-footer style="height: 50px; background: lightskyblue;">footer</g-footer>
+        </g-layout>
+                `
+                .replace(/^ {8}/gm, "")
+                .trim()
+        };
+    }
+};
+</script>
+<style lang="scss" scoped>
+* {
+    box-sizing: border-box;
+}
+</style>
--- docs/components/layout.md
+++ docs/components/layout.md
@@ -3,3 +3,8 @@ title: Layout
 sidebarDepth: 2
 ---
 # 布局
+<ClientOnly>
+<layout-demo-1/>
+<layout-demo-2/>
+<layout-demo-3/>
+</ClientOnly>
